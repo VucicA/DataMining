d41b597afab93c210466945c098d59aae477b876
VucicA <antoine.vucic@limayrac.fr>
2022-07-15 18:51:25 +0200

    Connect  to Mongo

--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 import tweepy
+import pymongo
 import config
 
 client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
@@ -7,6 +8,27 @@ query = 'from:Toulouse FC -is:retweet'
 
 response = client.search_recent_tweets(query=query, max_results=100)
 
+tweets = []
+
+
 for tweet in response.data:
-    print(tweet.id)
-    
+    tweets.append(tweet)
+
+
+myclient = pymongo.MongoClient(config.URL + ":" + config.PORT + "/")
+
+
+dblist = myclient.list_database_names()
+if "dataming" in dblist:
+    print("The database exists.")
+else :
+    mydb = myclient["dataming"]
+
+
+collist = mydb.list_collection_names()
+if "TFC" in collist:
+  print("The collection exists.")
+else:
+    mycol = mydb["TFC"]
+
+mycol.insert_one(tweets)
