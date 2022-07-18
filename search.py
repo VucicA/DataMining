f0b432d530c580483beb80c6eb5b2cf3306484a7
VucicA <antoine.vucic@gmail.com>
2022-07-18 11:22:51 +0200

    ready for the pres

--- search.py
+++ search.py
@@ -2,24 +2,36 @@ import tweepy
 import pymongo
 import config
 import json
+from pprint import pprint
 
-mongoDB = pymongo.MongoClient(config.URL + ":" + config.PORT + "/")
+mongoDB = pymongo.MongoClient("mongodb+srv://VucicA:7cCFuJ1udrMNe0OY@tfc.jcz2iy9.mongodb.net/?retryWrites=true&w=majority")
 mydb = mongoDB.datamining
 mycol = mydb.TFC
 
 twitter = tweepy.Client(bearer_token=config.BEARER_TOKEN)
 
-query = '"Toulouse FC" lang:fr -is:retweet'
+query = '"Toulouse FC" lang:fr -is:retweet '
 
-response = twitter.search_recent_tweets(query=query, max_results=10)
+response = twitter.search_recent_tweets(query=query, 
+                                        max_results=100, #max 100
+                                        tweet_fields=[
+                                            'text',
+                                            'public_metrics',
+                                            'created_at',
+                                            'geo'
+                                        ])
 
 tweets = response.data
-
 
 for tweet in tweets:
     sendJson = {
         'id' : tweet.id,
-        'text' : tweet.text
+        'text' : tweet.text,
+        'geo' :tweet.geo,
+        'created_at' : tweet.created_at,
+        'public_metrics' : tweet.public_metrics
     }
-    print(sendJson)
+
+    pprint(sendJson)
+
     mycol.insert_one(sendJson)
